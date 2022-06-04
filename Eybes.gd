6658445066b202e507d7a97b2dbcb43b071d09df
tompasini <pasinitom91@gmail.com>
2022-06-03 21:10:42 -0600

    eybes collisions and walk animation

--- Eybes.gd
+++ Eybes.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 var velocity = Vector2()
 export var direction = -1
 export var detects_cliffs = true
+var speed = 50
 
 func _ready():
 	if(direction == 1):
@@ -19,6 +20,16 @@ func  _physics_process(delta):
 	
 	velocity.y += 20
 	
-	velocity.x = 50 * direction
+	velocity.x = speed * direction
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
+
+
+func _on_TopChecker_body_entered(body):
+	if(body.name == 'LZer'):
+		$AnimatedSprite.play('squished')
+		speed = 0
+		set_collision_layer_bit(4, false)
+		set_collision_mask_bit(0, false)
+		$TopChecker.set_collision_layer_bit(4, false)
+		$TopChecker.set_collision_mask_bit(0, false)
