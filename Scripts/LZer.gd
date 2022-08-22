811a889b8294cadd99d0c1cc0a7ef2cada73dd6d
tompasini <pasinitom91@gmail.com>
2022-08-22 12:40:03 -0600

    beginning of wall jump functionality

--- Scripts/LZer.gd
+++ Scripts/LZer.gd
@@ -11,45 +11,46 @@ const JUMPFORCE = -550
 const GRAVITY = 35
 const LASER = preload("res://Laser.tscn")
 
-signal dmg_power_up_collected
-
 signal finished_level
 
 func _physics_process(delta):
 	if(alive && !finished):
 		if (Input.is_action_pressed("right")):
 			direction = 1
-			if(Input.is_action_pressed("run")):
-				speedMultiplier = 2
-			else:
-				speedMultiplier = 1
-			velocity.x = (SPEED * speedMultiplier)
-			$AnimatedSprite.flip_h = false
-			move()
+			if(!is_on_wall()):
+				if(Input.is_action_pressed("run")):
+					speedMultiplier = 2
+				else:
+					speedMultiplier = 1
+				velocity.x = (SPEED * speedMultiplier)
+				$AnimatedSprite.flip_h = false
+				move()
 		elif(Input.is_action_pressed("left")):
 			direction = -1
-			if(Input.is_action_pressed("run")):
-				speedMultiplier = 2
-			velocity.x = (-SPEED * speedMultiplier)
-			$AnimatedSprite.flip_h = true
-			move()
+			if(!is_on_wall()):
+				if(Input.is_action_pressed("run")):
+					speedMultiplier = 2
+				velocity.x = (-SPEED * speedMultiplier)
+				$AnimatedSprite.flip_h = true
+				move()
 		else:
 			$AnimatedSprite.play('idle')	
 		
-		if not is_on_floor():
+		if(!is_on_floor()):
 			$AnimatedSprite.play('jump')
 			
 		if(Input.is_action_just_pressed("shoot")):
 			shoot()
 		
-		velocity.y += GRAVITY
-
-		if(Input.is_action_just_pressed("jump") and is_on_floor()):
-			if(speedMultiplier == 1):
-				velocity.y = JUMPFORCE
-			else:
-				velocity.y = -700
-				velocity.x += (700 * direction)
+		if(!is_on_wall()):
+			velocity.y += GRAVITY
+		else:
+			if(velocity.y < 0):
+				velocity.y = 0
+			velocity.y += 5
+		
+		if((Input.is_action_just_pressed("jump") && is_on_floor()) || (Input.is_action_just_pressed("jump") && is_on_wall())):
+			jump()
 
 		velocity = move_and_slide(velocity, Vector2.UP)
 		
@@ -59,9 +60,6 @@ func _physics_process(delta):
 
 func _on_FallZone_body_entered(body):
 	get_tree().reload_current_scene()
-	
-func get_damage():
-	return (GlobalVariables.dmgPowerUps * damage) if (GlobalVariables.dmgPowerUps) else damage
 
 func bounce():
 	velocity.y = JUMPFORCE * 0.5
@@ -86,6 +84,16 @@ func move():
 		$AnimatedSprite.play("walk")
 	else:
 		$AnimatedSprite.play("run")
+
+func jump():
+	if(speedMultiplier == 1 && !is_on_wall()):
+		velocity.y = JUMPFORCE
+	elif(is_on_wall()):
+		velocity.y = -600
+		velocity.x += (600 * direction)
+	else:
+			velocity.y = -700
+			velocity.x += (700 * direction)
 
 func _on_Timer_timeout():
 	get_tree().reload_current_scene()
