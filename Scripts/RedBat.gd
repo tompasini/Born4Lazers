81df448b04bafec7849fe76707e2f67ac61524db
tompasini <pasinitom91@gmail.com>
2022-10-26 20:55:07 -0600

    red bat attack

--- Scripts/RedBat.gd
+++ Scripts/RedBat.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 var velocity = Vector2()
 
+var player_position = Vector2(0, 0)
+
 enum STATES {UP, DOWN, IDLE, ATTACK}
 
 var _state : int = STATES.UP
@@ -12,7 +14,13 @@ func _ready():
 func _physics_process(delta):
 	if(_state != STATES.ATTACK):
 		bob()
-	move_and_slide(velocity)
+		move_and_slide(velocity)
+	else:
+		attack(delta)
+		
+	if(position == player_position):
+		_state = STATES.UP
+
 
 func bob():
 	if($Timer.is_stopped()):
@@ -30,9 +38,9 @@ func bob():
 			velocity.y = lerp(velocity.y, 0, 1.0)
 		velocity.y += 0.15
 
-func attack(position):	
-	print('attack!')
+func attack(delta):
+	position = position.move_toward(player_position, delta * 150)
 
 func _on_Area2D_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	_state = STATES.ATTACK
-	attack(body.position)
+	player_position = Vector2(body.position.x, body.position.y)
