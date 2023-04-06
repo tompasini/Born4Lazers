dda1bfd64d0bb3f7eef0365c7d1e5c4b5a45d4a8
tompasini <pasinitom91@gmail.com>
2023-04-06 16:28:09 -0600

    lasers damage king eyebes, and kill when life is zero

--- Scripts/KingEyebes.gd
+++ Scripts/KingEyebes.gd
@@ -2,27 +2,25 @@ extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+var life = 50
 
 
 func _on_Body_body_entered(body):
 	if(body.name == 'LZer'):
 		body.hurt()
 	elif(body.name == 'Laser'):
-		$HitAura.visible = true
-		$HitAuraTimer.start()
-
+		if(life):
+			life -= GlobalVariables.laser_damage
+			body.queue_free()
+			$HitAura.visible = true
+			$HitAuraTimer.start()
+		if(!life):
+			$AnimatedSprite.play("dead")
+			remove_collisions()
 
 func _on_HitAuraTimer_timeout():
 	$HitAura.visible = false
+
+func remove_collisions():
+	$Body.set_collision_mask_bit(0, false)	
+	$Body.set_collision_mask_bit(5, false)
