0415161bbaac807d35cf6af94599d5882585ec9e
tompasini <pasinitom91@gmail.com>
2023-04-08 12:46:51 -0600

    removed unused assets, added dash

--- Scripts/KingEyebes.gd
+++ Scripts/KingEyebes.gd
@@ -6,17 +6,22 @@ var velocity = Vector2(0,0)
 var life = 50
 var eyebes_direction = -1
 var jump_direction = -1
+var dash_direction = 1
+var action = 1
+var has_dashed = false
 const EYEBES = preload("res://Enemies/Eyebes.tscn")
 const GRAVITY = 35
 
-enum STATES {IDLE, JUMPING, DEAD}
+enum STATES {IDLE, JUMPING, DASHING, DEAD}
 
 var _state = STATES.IDLE
 
 func _physics_process(delta):
 	if(_state != STATES.DEAD):
-		if(is_on_floor()):
+		if(is_on_floor() && velocity.x == 0):
 			_state = STATES.IDLE
+		elif(is_on_floor() && velocity.x != 0):
+			_state = STATES.DASHING
 		else:
 			_state = STATES.JUMPING
 	
@@ -24,12 +29,19 @@ func _physics_process(delta):
 	if(_state == STATES.IDLE):
 		$AnimatedSprite.play("idle")
 	if(_state == STATES.JUMPING):
+		if(velocity.y == -600):
+			velocity.x = (600 & jump_direction)
 		$AnimatedSprite.play("jump")
+	if(_state == STATES.DASHING):
+		if(dash_direction == 1):
+			$AnimatedSprite.play("dash_right")
+		else:
+			$AnimatedSprite.play("dash_left")
 	if(_state == STATES.DEAD):
 		$AnimatedSprite.play("dead")
 	velocity = move_and_slide(velocity, Vector2.UP)
-	if(is_on_floor()):
-		velocity.x = lerp(velocity.x, 0, 0.9)
+	if(is_on_floor() && _state != STATES.DASHING):
+		velocity.x = 0
 
 func _on_Body_body_entered(body):
 	if(body.name == 'LZer'):
@@ -44,6 +56,7 @@ func _on_Body_body_entered(body):
 		if(!life):
 			_state = STATES.DEAD
 			remove_collisions()
+			$HitAura.visible = false
 
 func _on_HitAuraTimer_timeout():
 	$HitAura.visible = false
@@ -63,8 +76,25 @@ func spawn_eyebes():
 func jump():
 	velocity.y = -600
 	velocity.x = (600 * jump_direction)
-	jump_direction = jump_direction * -1
+	if(has_dashed):
+		dash_direction = dash_direction * -1
+	if(dash_direction == 1):
+		jump_direction = -1
+	else:
+		jump_direction = 1
+	action = 2
+
+func dash():
+	velocity.x = (700 * dash_direction)
+	$DashTimer.start()
+	action = 1
 
 func _on_AttackTimer_timeout():
 	if(_state != STATES.DEAD):
-		jump()
+		match action:
+			1:
+				jump()
+			2:
+				dash()
+				if(!has_dashed):
+					has_dashed = true
