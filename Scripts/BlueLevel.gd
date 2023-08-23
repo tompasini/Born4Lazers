8bfd4f19f4196eee5ce5c5b7b5474641a4fead82
tompasini <pasinitom91@gmail.com>
2023-08-23 09:44:52 -0600

    blue levels templated

--- Scripts/BlueLevel.gd
+++ Scripts/BlueLevel.gd
@@ -2,3 +2,10 @@ extends Node2D
 
 func _ready():
 	GlobalVariables.currentWorld = "Blue"
+
+
+func _on_BlueSwitch_body_entered(body):
+	$LZer.remove_collisions()
+	$LZer.finished = true
+	$BlueSwitch.get_node("AnimatedSprite").play("pressed")
+	$LZer.get_node("AnimatedSprite").play("finished")
