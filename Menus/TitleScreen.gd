2f3ee4fa5b347d213b347081a121eaf438a96a56
tompasini <pasinitom91@gmail.com>
2023-09-04 19:32:48 -0600

    setup title menu ui

--- Menus/TitleScreen.gd
+++ Menus/TitleScreen.gd
@@ -1,23 +1,57 @@
 extends MarginContainer
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var yellow = Color8(247, 247, 10, 255)
+var default = Color(1, 1, 1, 1)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$HBoxContainer/VBoxContainer/Play.grab_focus()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func _on_Play_focus_entered():
-	print('focused on Play')
+	$HBoxContainer/VBoxContainer/Play.modulate = yellow
 
 
 func _on_Level_Select_focus_entered():
-	print('focused on Level Select')
+	$"HBoxContainer/VBoxContainer/Level Select".modulate = yellow
+
+
+func _on_Play_mouse_entered():
+	$HBoxContainer/VBoxContainer/Play.grab_focus()
+
+
+func _on_Level_Select_mouse_entered():
+	$"HBoxContainer/VBoxContainer/Level Select".grab_focus()
+
+
+func _on_Play_focus_exited():
+	$HBoxContainer/VBoxContainer/Play.modulate = default
+
+
+func _on_Level_Select_focus_exited():
+	$"HBoxContainer/VBoxContainer/Level Select".modulate = default
+
+
+func _on_Options_focus_entered():
+	$HBoxContainer/VBoxContainer/Options.modulate = yellow
+
+
+func _on_Options_focus_exited():
+	$HBoxContainer/VBoxContainer/Options.modulate = default
+
+
+func _on_Options_mouse_entered():
+	$HBoxContainer/VBoxContainer/Options.grab_focus()
+
+
+func _on_Quit_focus_entered():
+	$HBoxContainer/VBoxContainer/Quit.modulate = yellow
+
+
+func _on_Quit_focus_exited():
+	$HBoxContainer/VBoxContainer/Quit.modulate = default
+
+
+func _on_Quit_mouse_entered():
+	$HBoxContainer/VBoxContainer/Quit.grab_focus()
